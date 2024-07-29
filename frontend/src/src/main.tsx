b6c47d0f8aa712e38fa1f3b98032d5f62f28bf2e
h4shu <12460080+h4shu@users.noreply.github.com>
2024-07-29 16:03:28 +0900

    rename app/ to src/

--- frontend/src/src/main.tsx
+++ frontend/src/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider, Container, ColorModeScript } from '@chakra-ui/react'
-import theme from './theme'
+import theme from './theme.tsx'
 import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
